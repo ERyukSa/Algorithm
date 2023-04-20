307356d24cb453597f212868e82087978d09f703
ERyukSa <jslkap227@gmail.com>
2023-04-20 20:39:15 +0900

    백준 2294번 - 동전 2 재도전

--- Dynamic Programming/백준 2294번 - 동전 2.kt
+++ Dynamic Programming/백준 2294번 - 동전 2.kt
@@ -1,31 +1,31 @@
+package src.week6
+
 /**
  * https://www.acmicpc.net/problem/2294
- * DP를 공부하자.......!!
+ * 1차) DP를 공부하자.....!! (틀림)
+ * 2차) 계속 공부하자
  */
 
 fun main() {
-    val (n, k) = readln().split(" ").map(String::toInt)
-    val dpTable = Array(n + 1) {
-        IntArray(k + 1) { n + 1 }
+    val (n, goalValue) = readln().split(" ").map(String::toInt)
+    val minCountFor = IntArray(goalValue + 1) { goalValue + 1 }.apply { this[0] = 0 }
+    val coins: List<Int> = run {
+        val coinSet = HashSet<Int>()
+        repeat(n) {
+            coinSet.add(readln().toInt())
+        }
+        coinSet.sorted()
     }
-    val coinSet = HashSet<Int>()
-    repeat(n) {
-        coinSet.add(readln().toInt())
-    }
-    val coins = coinSet.toIntArray()
 
-    for (coinCount in 1..n) {
-        for (goalSum in 1..k) {
-            dpTable[coinCount][goalSum] = dpTable[coinCount - 1][goalSum]
-            if (goalSum < coins[coinCount - 1]) continue
-            dpTable[coinCount][goalSum] =
-                minOf(dpTable[coinCount][goalSum],dpTable[coinCount - 1][goalSum - coins[coinCount - 1]] + 1)
+    for (coin in coins) {
+        for (value in coin..goalValue) {
+            minCountFor[value] = minOf(minCountFor[value], minCountFor[value - coin] + 1)
         }
     }
 
-    if (dpTable[n][k] == n + 1) {
+    if (minCountFor[goalValue] == goalValue + 1) {
         print(-1)
     } else {
-        print(dpTable[n][k])
+        print(minCountFor[goalValue])
     }
 }
