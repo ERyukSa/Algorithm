f5bc7cc9ac13b994333681e4730bb0650507d05a
ERyukSa <jslkap227@gmail.com>
2023-04-24 23:18:05 +0900

    백준 11049번 - 행렬 곱셈 순서 (2차)

--- Dynamic Programming/백준 11049번 - 행렬 곱셈 순서.kt
+++ Dynamic Programming/백준 11049번 - 행렬 곱셈 순서.kt
@@ -1,6 +1,6 @@
 /**
  * https://www.acmicpc.net/problem/11049
- * DP, 1차: 1% fail
+ * DP, 1차, 2차: 1% fail
  */
 data class Matrix(val rowSize:Int, val colSize: Int)
 
@@ -14,38 +14,59 @@ fun main() {
             Matrix(rowSize = this[0].toInt(), colSize = this[1].toInt())
         }
     }
+
     minTimesCount = Array(matrixCount) {
         IntArray(matrixCount) { -1 }
+    }
+    for (i in 0 until matrixCount - 1) {
+        minTimesCount[i][i + 1] = matrices[i].rowSize * matrices[i].colSize * matrices[i + 1].colSize
     }
 
     calculateMinTimesCount(0, matrixCount - 1).let { print(it) }
 }
 
 private fun calculateMinTimesCount(start: Int, end: Int): Int {
-    if (start == end) {
+    if (start >= end) {
         return 0
-    }
-    if (start + 1 == end) {
-        return matrices[start].rowSize * matrices[start].colSize * matrices[end].colSize
     }
     if (minTimesCount[start][end] != -1) {
         return minTimesCount[start][end]
     }
 
     var myMinTimesCount = Int.MAX_VALUE
-    for (i in start + 1 until end) {
-        val leftMinTimesCount = calculateMinTimesCount(start, i - 1)
-        val rightMinTimesCount = calculateMinTimesCount(i + 1, end)
-        val leftFirstTimesCount = (matrices.first().rowSize * matrices[i].rowSize * matrices[i].colSize) +
-                matrices.first().rowSize * matrices[i].colSize * matrices.last().colSize
-        val rightFirstTimesCount = (matrices[i].rowSize * matrices[i].colSize * matrices.last().colSize) +
-                matrices.first().rowSize * matrices[i].rowSize * matrices.last().colSize
+    for (i in start until end) {
+        val leftMinCount = calculateMinTimesCount(start, i - 1)
+        val rightMinCount = calculateMinTimesCount(i + 2, end)
+        val adjacentCount = matrices[i].rowSize * matrices[i].colSize * matrices[i + 1].colSize
+
         myMinTimesCount = minOf(
             myMinTimesCount,
-            leftMinTimesCount + minOf(leftFirstTimesCount, rightFirstTimesCount) + rightMinTimesCount
+            leftMinCount + adjacentCount + rightMinCount + getThreeSequentialTimesCount(start, i, i + 1, end)
         )
     }
 
     minTimesCount[start][end] = myMinTimesCount
     return myMinTimesCount
+}
+
+private fun getThreeSequentialTimesCount(start: Int, mid: Int, end: Int): Int {
+    return minOf(
+        matrices[start].rowSize * matrices[start].colSize * matrices[mid].colSize +
+                matrices[start].rowSize * matrices[mid].colSize * matrices[end].colSize,
+        matrices[mid].rowSize * matrices[end].rowSize * matrices[end].colSize +
+                matrices[start].rowSize * matrices[mid].colSize * matrices[end].colSize,
+    )
+}
+
+private fun getThreeSequentialTimesCount(start: Int, mid1: Int, mid2:Int, end: Int): Int {
+    return when {
+        start == mid1 -> matrices[mid1].rowSize * matrices[mid2].colSize * matrices[end].colSize
+        end == mid2 -> matrices[start].rowSize * matrices[mid1].rowSize * matrices[mid2].colSize
+        else -> minOf(
+            matrices[start].rowSize * matrices[mid1].rowSize * matrices[mid2].colSize +
+                    matrices[start].rowSize * matrices[mid2].colSize * matrices[end].colSize,
+            matrices[mid1].rowSize * matrices[mid2].colSize * matrices[end].colSize +
+                    matrices[start].rowSize * matrices[mid1].rowSize * matrices[end].colSize,
+        )
+    }
 }
