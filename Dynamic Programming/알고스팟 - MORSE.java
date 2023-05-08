cac6ab75cf725bd7c262101a99e14185d5ee5e07
ERyukSa <jslkap227@gmail.com>
2023-05-08 15:20:33 +0900

    알고스팟 - MORSE (new)

--- Dynamic Programming/알고스팟 - MORSE.java
+++ Dynamic Programming/알고스팟 - MORSE.java
@@ -1,66 +1,70 @@
 /**
  * https://www.algospot.com/judge/problem/read/NUMB3RS
- * DP(k번째 답 구하기), 종만북
+ * DP(k번째 답 구하기), 종만북, 어렵다
  */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Main {
 
-    private static int maxBino = 1_000_000_000; // 이항계수로 계산할 최대 값
-    private static int[][] bino = new int[201][201]; // 이항 계수
-    private static int skipCount = 0;
+    private static int[][] signCount;
+
+    private static int MAX_SIGN_COUNT = 1_000_000_001;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int t = Integer.parseInt(br.readLine());
-        calculateBino();
+        signCount = new int[101][101];
+        for (int i = 0; i <= 100; i++) {
+            Arrays.fill(signCount[i], -1);
+        }
 
-        while (t-- > 0) {
+        for (int t = Integer.parseInt(br.readLine()); t > 0; t--) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int longCount = Integer.parseInt(st.nextToken());
             int shortCount = Integer.parseInt(st.nextToken());
-            skipCount = Integer.parseInt(st.nextToken()) - 1;
-
-            makeTargetMos(longCount, shortCount, "");
+            int skipCount = Integer.parseInt(st.nextToken()) - 1;
+            sb.append(getTargetSign(longCount, shortCount, skipCount));
+            sb.append("\n");
         }
+
+        System.out.print(sb);
     }
 
-    private static void calculateBino() {
-        for (int i = 0; i <= 200; i++) {
-            for (int j = 0; j <= 200; j++) {
-                bino[i][j] = 0;
-            }
+    // 입력 부호 개수로 만들 수 있는 모든 신호의 개수 반환, 이항 계수 원리와 같음
+    private static int getSignCount(int longCount, int shortCount) {
+        if (longCount == 0 || shortCount == 0) {
+            return 1;
+        }
+        if (signCount[longCount][shortCount] != -1) {
+            return signCount[longCount][shortCount];
         }
 
-        for (int i = 0; i <= 200; i++) {
-            bino[i][0] = bino[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                bino[i][j] = Math.min(maxBino, bino[i - 1][j - 1] + bino[i - 1][j]);
-            }
-        }
+        signCount[longCount][shortCount] =
+                getSignCount(longCount - 1, shortCount) + getSignCount(longCount, shortCount - 1);
+        // 이항 계수 계산 중 오버플로우가 나지 않도록 최대 값을 K + 1(MAX_SIGN_COUNT)로 제한한다.
+        // 아래 재귀 함수에서 부호를 하나 선택할 때마다 건너뛸지 말지 결정하는데,
+        // 기준은 그 부호로 만들 수 있는 신호의 개수가 skipCount보다 큰지 여부이다. (skipCount의 최대 값은 K)
+        signCount[longCount][shortCount] = Math.min(MAX_SIGN_COUNT, signCount[longCount][shortCount]);
+        return signCount[longCount][shortCount];
     }
 
-    private static void makeTargetMos(int longCount, int shortCount, String mos) {
-        if (skipCount < 0) {
-            return;
-        }
-        if (longCount == 0 && shortCount == 0) {
-            if (skipCount == 0) {
-                System.out.println(mos);
+    private static String getTargetSign(int longCount, int shortCount, int skipCount) {
+        if (longCount == 0) {
+            StringBuilder sb =  new StringBuilder();
+            for (int i = 0; i < shortCount; i++) {
+                sb.append('o');
             }
-            skipCount--;
-            return;
+            return sb.toString();
         }
 
-        if (bino[longCount + shortCount][longCount] <= skipCount) {
-            skipCount -= bino[longCount + shortCount][longCount];
-            return;
+        int signCountStartingLong = getSignCount(longCount - 1, shortCount);
+        if (skipCount < signCountStartingLong) {
+            return "-" + getTargetSign(longCount - 1, shortCount, skipCount);
         }
-        if (longCount > 0) makeTargetMos(longCount - 1, shortCount, mos + "-");
-        if (shortCount > 0) makeTargetMos(longCount, shortCount - 1, mos + "o");
+        return "o" + getTargetSign(longCount, shortCount - 1, skipCount - signCountStartingLong);
     }
 }
